d8b9958a3597250dcbe2f26f2ea5bd2b3a59ce87
Andrejus <andrejus@think-engineer.com>
2018-04-06 22:07:54 +0300

    Add standard user input

--- fibonacci-fork.c
+++ fibonacci-fork.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
-int main() {
-    fork();
+int main(int argc, char *argv[]) {
+
     printf("Hello, world!\n");
+
+    printf("Passed argument count: %d\n", argc);
+
+    int number;
+
+    /* If no argument */
+    if (argc <= 1) {
+        /* Get argument */
+        printf("No argument given. Enter arguemnt:\n");
+        scanf("%d", &number);
+    } else {
+        number = atoi(argv[1]);
+    }
+
+    printf("Passed argument: %d\n", number);
+
+    //pid_t process = fork();
+
     return 0;
 }
