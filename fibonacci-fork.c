c1885222211781127619aadddd83fc56e341a048
Andrejus <andrejus@think-engineer.com>
2018-04-06 22:47:50 +0300

    Clean up whitespace
    at least it was an a4

--- fibonacci-fork.c
+++ fibonacci-fork.c
@@ -2,58 +2,45 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-unsigned long calculateFibonacci(int number) {
+unsigned long calculateFibonacci(int num) {
     /* Base case */
-    if (number < 1) {
+    if (num < 1) {
         return 0;
-    }
-    else if (number < 2) {
+    } else if (num < 2) {
         return 1;
     }
     /* Recurse */ 
     else {
-        return calculateFibonacci(number - 1) + calculateFibonacci(number - 2);
+        return calculateFibonacci(num - 1) + calculateFibonacci(num - 2);
     }
 }
 
 int main(int argc, char *argv[]) {
-
     int number;
-
     while (1) {
         /* Get argument */
         printf("Enter arguemnt (-1 to exit):\n");
         scanf("%d", &number);
-
         /* Exit condition */
         if (number < 0) {
             break;
         }
-
         pid_t pid = fork();
-
         /* Child process */
         if (pid == 0) {
-            /* Calculate fibonacci */
             unsigned long fibonacci = calculateFibonacci(number);
             printf("Child process returned: %lu\n", fibonacci);
             return 0;
         }
-
         /* Parent process */ 
         else if (pid > 0) {
-            /* Continue serving */
             printf("Continuing parent process... ");
         }
-
         /* Failed to create */
         else if (pid < 0) {
             printf("Failed to create child process.\n");
             return 1;
         }
-
     }
-    
     return 0;
-
 }
