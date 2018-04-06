84f4e20cd2140386d1baeba78c52186357d54a8a
Andrejus <andrejus@think-engineer.com>
2018-04-06 22:19:43 +0300

    Implement fibonacci fork call

--- fibonacci-fork.c
+++ fibonacci-fork.c
@@ -2,11 +2,18 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+int calculateFibonacci(int number) {
+    /* Base case */
+    if (number <= 2) {
+        return 1;
+    }
+    /* Recurse */ 
+    else {
+        return calculateFibonacci(number - 1) + calculateFibonacci(number - 2);
+    }
+}
+
 int main(int argc, char *argv[]) {
-
-    printf("Hello, world!\n");
-
-    printf("Passed argument count: %d\n", argc);
 
     int number;
 
@@ -18,6 +25,28 @@ int main(int argc, char *argv[]) {
         /* Exit condition */
         if (number < 0) {
             break;
+        }
+
+        pid_t pid = fork();
+
+        /* Child process */
+        if (pid == 0) {
+            /* Calculate fibonacci */
+            int fibonacci = calculateFibonacci(number);
+            printf("Child process returned: %d\n", fibonacci);
+            return 0;
+        }
+
+        /* Parent process */ 
+        else if (pid > 0) {
+            /* Continue serving */
+            printf("Parent process: ");
+        }
+
+        /* Failed to create */
+        else if (pid < 0) {
+            printf("Failed to create child process.\n");
+            return 1;
         }
 
 
