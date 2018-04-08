38b8c22e904d0226a39773e7242e7892a6fcd2f5
Andrejus <andrejus@think-engineer.com>
2018-04-08 23:09:23 +0300

    Add minimal hello world async programs

---
+++ login-handler.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("login-handler\n");
+    return 0;
+}
---
+++ login-system.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("login-system\n");
+    return 0;
+}
